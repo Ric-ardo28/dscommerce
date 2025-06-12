ddf281c46e0ba9c75a79dc515b237a32b0bdaf8a
Ricardo Rodrigues Santana <140956804+Ric-ardo28@users.noreply.github.com>
2025-06-11 21:36:52 -0300

    Ordem, Enum, relacionamento muitos-para-um DSCommerce - DevSuperior

---
+++ src/main/java/br/com/ricardo/dscommerce/entities/OrderStatus.java
@@ -0,0 +1,10 @@
+package br.com.ricardo.dscommerce.entities;
+
+public enum OrderStatus {
+	WAITING_PAYMENT,
+	PAID,
+	SHIPPED,
+	DELIVERED,
+	CANCELLED,
+
+}
---
+++ src/main/java/br/com/ricardo/dscommerce/entities/User.java
@@ -0,0 +1,33 @@
+package br.com.ricardo.dscommerce.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_user")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String name;
+	private String email;
+	private String phone;
+	private LocalDate birthDate;
+	private String password;
+
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+
+}
