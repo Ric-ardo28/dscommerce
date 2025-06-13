194e5e859e3c7bf99f90e6ecc8d35c87ce3511ef
Ricardo Rodrigues Santana <140956804+Ric-ardo28@users.noreply.github.com>
2025-06-12 21:22:26 -0300

    Pagamento, relacionamento um-para-um DSCommerce - DevSuperior

--- src/main/java/br/com/ricardo/dscommerce/entities/Payment.java
+++ src/main/java/br/com/ricardo/dscommerce/entities/Payment.java
@@ -5,30 +5,25 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
 import java.time.Instant;
 
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 @Entity
-@Table(name = "tb_order")
-public class Order {
+@Table(name = "tb_payment")
+public class Payment {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
-	private Instant	moment;
+	private Instant moment;
 
-	private OrderStatus status;
-
-	@ManyToOne
-	@JoinColumn(name = "client_id")
-	private User client;
-
-	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
-	private Payment payment;
-
+	@OneToOne
+	@MapsId
+	private Order order;
 }
