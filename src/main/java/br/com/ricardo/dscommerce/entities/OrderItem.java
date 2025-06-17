5929ef3bea3f86ecac7b47f113e06f00e9a3de69
Ricardo Rodrigues Santana <140956804+Ric-ardo28@users.noreply.github.com>
2025-06-16 22:28:48 -0300

    Muitos-para-muitos, coluna única e texto DSCommerce - DevSuperior

---
+++ src/main/java/br/com/ricardo/dscommerce/entities/OrderItem.java
@@ -0,0 +1,25 @@
+package br.com.ricardo.dscommerce.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class OrderItem {
+
+	private OrderItemPK id = new OrderItemPK();
+
+	private Integer quantity;
+	private Double price;
+
+	public OrderItem(Order order,Product product, Integer quantity, Double price) {
+		id.setOrder(order);
+		id.setProduct(product);
+		this.quantity = quantity;
+		this.price = price;
+	}
+}
---
+++ src/main/java/br/com/ricardo/dscommerce/entities/OrderItemPK.java
@@ -0,0 +1,25 @@
+package br.com.ricardo.dscommerce.entities;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Embeddable
+public class OrderItemPK {
+	@ManyToOne
+	@JoinColumn(name = "order_id")
+	private Order order;
+
+	@ManyToOne
+	@JoinColumn(name = "product_id")
+	private Product product;
+}
