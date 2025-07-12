8592f4638d0681868ed78a41efa0e902c8a3c4c0
Ricardo Rodrigues Santana <140956804+Ric-ardo28@users.noreply.github.com>
2025-07-11 23:31:06 -0300

    Validação com Bean Validation DSCommerce - DevSuperior

--- src/main/java/br/com/ricardo/dscommerce/controllers/ProductController.java
+++ src/main/java/br/com/ricardo/dscommerce/controllers/ProductController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.util.List;
+
 
 // Indica que esta classe é um controller REST
 @RestController
