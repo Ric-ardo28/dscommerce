dc41abdc118ca9422eeec5716bb21db338d3e910
Ric-ardo28 <ricardo2803br@hotmail.com>
2026-06-09 22:35:29 -0300

    Iniciando a estrutura do projeto DevSuperior DScommerce

---
+++ src/main/java/com/ricardodev/dscommerce/entities/Order.java
@@ -0,0 +1,11 @@
+package com.ricardodev.dscommerce.entities;
+
+import java.time.Instant;
+
+public class Order {
+    private Instant moment;
+    private OrderStatus status;
+    private User client;
+
+
+}
---
+++ src/main/java/com/ricardodev/dscommerce/entities/User.java
@@ -0,0 +1,32 @@
+package com.ricardodev.dscommerce.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private LocalDate birthDate;
+    private String password;
+
+
+
+
+
+
+
+}
