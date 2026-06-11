f5e3c36dead3086271f100f1347a58613fcc9579
Ric-ardo28 <ricardo2803br@hotmail.com>
2026-06-11 19:44:51 -0300

    Iniciando a estrutura do projeto DevSuperior DScommerce "Order"

---
+++ src/main/java/com/ricardodev/dscommerce/entities/Payment.java
@@ -0,0 +1,22 @@
+package com.ricardodev.dscommerce.entities;
+
+import jakarta.persistence.*;
+import org.springframework.aot.generate.GeneratedTypeReference;
+
+import java.time.Instant;
+@Entity
+@Table(name = "tb_payment")
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant moment;
+
+    @OneToOne
+    @MapsId
+    private Order order;
+
+
+}
