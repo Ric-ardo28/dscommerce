c89cb2cad39855fca8fa54b0731e975015e02a29
Ric-ardo28 <ricardo2803br@hotmail.com>
2026-06-18 21:42:49 -0300

    Adicionar ProductService e ProductDTO, refatorar ProductController para usar o serviço

--- src/main/java/com/ricardodev/dscommerce/controllers/ProductController.java
+++ src/main/java/com/ricardodev/dscommerce/controllers/ProductController.java
@@ -1,25 +1,25 @@
 package com.ricardodev.dscommerce.controllers;
 
-import com.ricardodev.dscommerce.entities.Product;
-import com.ricardodev.dscommerce.repositories.ProductRepository;
+import com.ricardodev.dscommerce.dto.ProductDTO;
+import com.ricardodev.dscommerce.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
+
+
 
 @RestController
 @RequestMapping(value = "/products")
 public class ProductController {
     @Autowired
-    private ProductRepository repository;
+    private ProductService service;
 
-    @GetMapping
-    public String teste(){
-       Optional<Product> result = repository.findById(1L);
-       Product product = result.get();
-       return product.getName();
+    @GetMapping(value = "/{id}")
+    public ProductDTO findById(@PathVariable Long id){
+        return service.findById(id);
 
     }
 
---
+++ src/main/java/com/ricardodev/dscommerce/dto/ProductDTO.java
@@ -0,0 +1,27 @@
+package com.ricardodev.dscommerce.dto;
+
+import com.ricardodev.dscommerce.entities.Product;
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class ProductDTO {
+
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private String imgUrl;
+
+    public ProductDTO(Product entity) {
+        id = entity.getId();
+        name = entity.getName();
+        description = entity.getDescription();
+        price = entity.getPrice();
+        imgUrl = entity.getImgUrl();
+    }
+}
